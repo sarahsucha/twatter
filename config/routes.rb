6b5c965bafbf811dbc73e55542e5863262087b7d
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-09-30 12:02:45 -0700

    Add Twat Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'twat_pages/home'
+
+  get 'twat_pages/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#hello'
