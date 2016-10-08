710fa859919a917b45edba4ad1afeead50c28fab
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-10-08 14:59:09 -0700

    Add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'twat_pages#home'
 
-  get 'twat_pages/home'
+  get '/help', to: 'twat_pages#help'
 
-  get 'twat_pages/help'
+  get '/about', to: 'twat_pages#about'
 
-  get 'twat_pages/about'
-
-  get 'twat_pages/contact'
+  get '/contact', to: 'twat_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 
 end
