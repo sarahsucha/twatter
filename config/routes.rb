bea9930beaa18f1fed285a26546e8f0a4d455c07
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-10-08 15:08:40 -0700

    Update new user page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'twat_pages#contact'
 
+  get '/signup', to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
